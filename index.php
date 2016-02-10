da93df31c7a3463e43b2c2de5ae1e8efa8fc25ac
= <wallacemaxters@gmail.com>
2016-02-10 18:01:22 -0200

    :)

--- index.php
+++ index.php
@@ -10,39 +10,38 @@
 <body>
 
     <form class="wgm-form fluid">
-        
         <fieldset>
-
         <!-- <legend>Form Wallace</legend> -->
+            <div class="wgm-row">
+                <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
+                    <div class="wgm-input-group">
+                        <label>mês</label>
+                        <select>
+                        <?php foreach (range(1, 12) as $key => $value): ?>
+                            <option value="<?= $value ?>"><?= strftime('%B', mktime(0, 0, 0, $value, 1)) ?></option>
+                        <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
 
-        <div class="wgm-row">
-
-            <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
-
-                <div class="wgm-input-group">
-                    <label class="label-horizontal">mês</label>
-                    <select>
-                    <?php foreach (range(1, 12) as $key => $value): ?>
-                        <option value="<?= $value ?>"><?= strftime('%B', mktime(0, 0, 0, $value, 1)) ?></option>
-                    <?php endforeach; ?>
-                    </select>
+                <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
+                    <div class="wgm-input-group">
+                        <label >&#9993;</label>
+                        <input type="text" />
+                    </div>
                 </div>
                 
-            </div>
+                <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
+                    
+                    <div class="wgm-input-group">
+                        <label>Senha</label>
+                        <input type="password" />
+                    </div>
+                </div>
 
-            <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
-                <input type="text" />
             </div>
-            
-            <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
-                <input type="search" />
-            </div>
-
-        </div>
-
         </fieldset>
     
-
         <div class="wgm-row">
             
             <div class="wgm-grid-4 wgm-grid-sm-6 wgm-grid-xs-12">
@@ -65,10 +64,8 @@
         </div>
 
         <fieldset>
-        <div class="wgm-row">
-            
+            <div class="wgm-row">                
                 <div class="wgm-grid-4 wgm-grid-xs-12">
-
                     <input type="reset" name="Cancelar" />
                     <button type="button">Informação</button>
                     <button type="reset" name="Cancelar">Resetar</button>
@@ -76,7 +73,6 @@
                     <button type="submit">Aprovar</button>
                     <button type="submit">Aprovar Foto</button>
                 </div>
-
                 <div class="wgm-grid-8 wgm-grid-xs-12">
                     <select multiple="multiple">
                         <?php foreach (range(1, 12) as $key => $value): ?>
@@ -84,12 +80,19 @@
                         <?php endforeach; ?>
                     </select>
                 </div>
-            
-        </div>
-
+            </div>
         </fieldset>
 
     </form>
+
+    <div class="wgm-row">
+        <div class="wgm-grid-xs-6">
+            <button class="wgm-btn">WGM-BTN DEFAULT</button>
+            <button class="wgm-btn wgm-green">WGM-BTN GREEN</button>
+            <button class="wgm-btn wgm-red">WGM-BTN RED</button>
+            <button class="wgm-btn wgm-yellow">WGM-BTN YELLOW</button>
+        </div>
+    </div>
 
 </body>
 </html>
